8089eea4712a6834f33a315e464329ad1c18267c
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-03-26 21:50:36 +0100

    Footer data 2024

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
            <Image width={50} height={50} src="/logo.webp" alt="logo" />
            <span className='hidden sm:block md:text-2xl' style={{fontFamily:"Lavishly Yours"}}>{"Todo List"}</span>
            </div>
-        <p className='text-xs md:text-xl'>© 2023 Tekandme.All Rights Reserved.</p>
+        <p className='text-xs md:text-xl'>© 2024 Tekandme.All Rights Reserved.</p>
         <div className='gap-2 hidden sm:flex'>
         <span className="flex items-center justify-center h-9 w-9 border-2 border-gray-400 rounded-full cursor-pointer">
           <FontAwesomeIcon icon={faFacebookF} size="sm" />

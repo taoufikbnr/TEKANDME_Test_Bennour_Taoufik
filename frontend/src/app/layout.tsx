a6655a375de3d81d52abf66d74716db2ec7a03c5
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-03-26 21:47:53 +0100

    re-add vh

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,13 +25,16 @@ export default function RootLayout({
       <link href="https://fonts.googleapis.com/css2?family=Lavishly+Yours&display=swap" rel="stylesheet"/>
       </head>
       <body
-        className={` flex flex-col justify-between`}
+       
       >
         <UserProvider>
+          <div  className={`flex flex-col justify-between h-screen`}>
+
           <Header />
           <ToastContainer position="top-center" autoClose={3000} />
           {children}
           <Footer/>
+          </div>
         </UserProvider>
       </body>
     </html>
